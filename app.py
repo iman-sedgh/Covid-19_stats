1bd92c328d929b1df1ee89d3e9d9be8e897b3184
iman-sedgh <iman.sedgh@yahoo.com>
2020-05-24 20:42:40 +0430

    classificated successfully

--- app.py
+++ app.py
@@ -1,26 +1,22 @@
-print ("Corona Virus (COVID-19) Stats In Iran \n Made by Sam Abbasi and Iman Sedgh")
+"""Corona Virus (COVID-19) Stats In Iran \n Made by Sam Abbasi and Iman Sedgh"""
+
 from bs4 import BeautifulSoup
 import requests
-page = requests.get('https://www.worldometers.info/coronavirus/country/iran/')
-soup = BeautifulSoup(page.content,'html.parser')
-lst = soup.find_all(id='maincounter-wrap')
-div_cases= lst[0]
-span_cases= div_cases.find('span')
-number_cases = span_cases.get_text()
 
 
-div_death = lst[1]
-span_death = div_death.find('span')
-number_death = span_death.get_text()
+class Data:
+    def __init__(self):
+        self.url = "https://www.worldometers.info/coronavirus/country/iran/"
 
+    def get_data(self):
+        page = requests.get('https://www.worldometers.info/coronavirus/country/iran/')
+        soup = BeautifulSoup(page.content,'html.parser')
+        selected = soup.select("#maincounter-wrap span")
+        number_list = [ item.text for item in selected ]
+        return number_list 
 
+numbers = Data().get_data()
 
-div_recover = lst[2]
-span_recover = div_recover.find('span')
-number_recover = span_recover.get_text()
-
-
-
-print ("Number Of Corona Virus Cases:  " +  number_recover)
-print("Number Of Corona Virus Deaths: " + number_cases)
-print("Number Of Corona Virus Recovers: " + number_death)
+print ("Number Of Corona Virus Cases:  " +  numbers[0])
+print("Number Of Corona Virus Deaths: " + numbers[1])
+print("Number Of Corona Virus Recovers: " + numbers[2])
